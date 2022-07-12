13917ed2d92d57c000637ab40eb5d5acb5ae793c
Dick Dijk <dick@dijkrosoft.nl>
2022-07-12 10:39:41 +0200

    Add flag to print debug logging

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var fs = require('fs')
 
+const yargs  = require('yargs')
+
+const argv = yargs.option('debug', {
+           alias: 'd',
+           description: 'print debug logging',
+           type: 'boolean'
+         })
+         .help()
+         .alias('help','h').argv
+
 /**
   * Process Cucumber output file 'cucumber_report.json'
   */
 let data = fs.readFileSync('cucumber_report.json');
 let json = JSON.parse(data);
 let scenarios = json[0].elements;
-console.log(`Aantal scenarios is ${scenarios.length}`)
+
+if ( argv.debug ) console.log(`Aantal scenarios is ${scenarios.length}`)
 
 passed_scenarios = scenarios.filter(scen => scen.steps.every(e => e.result.status == 'passed'));
 passed_scenario_names = passed_scenarios.map(scen => scen.name)
-console.log(`Er zijn ${passed_scenarios.length} scenarios geslaagd`)
-console.log(passed_scenario_names)
-console.log('regel nummers van geslaagde scenarios:')
+if ( argv.debug ) console.log(`Er zijn ${passed_scenarios.length} scenarios geslaagd`)
+if ( argv.debug ) console.log(passed_scenario_names)
+if ( argv.debug ) console.log('regel nummers van geslaagde scenarios:')
 
 line_numbers = passed_scenarios.map(e => e.line)
 
-line_numbers.forEach(e => console.log(Number(e)-1))
+const cucumberFeatureFile = 'AppCampusGuide.feature';
+
+// output is 'sed' commands to delete '@Android' so this scenario is not run anymore
+line_numbers.forEach(e => console.log(`sed -e '${Number(e)-1}s/@Android//' cucumberFeatureFile`))
 
